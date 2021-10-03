83b51bec0cc74239a7673232ce39ceb8dee750df
vivek <vivekvgsk@gmail.com>
2021-10-03 17:36:07 +0530

    bday alert partially completed

--- emp/urls.py
+++ emp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignInView,EmpCreationView,EmpListView,EmpProfileView,EmpEditView,Home,sign_out,EmployeeFilterView
+from .views import SignInView,EmpCreationView,EmpListView,EmpProfileView,EmpEditView,Home,sign_out,EmployeeFilterView,bday_alert
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -14,6 +14,7 @@ urlpatterns = [
     path("profile/<int:pk>",login_required( EmpProfileView.as_view(),login_url='login'),name="profile"),
     path("profileedit/<int:pk>",login_required( EmpEditView.as_view(),login_url='login'),name="profileedit"),
     path("home",login_required(Home.as_view(),login_url='login') , name="home"),
-    path("search",login_required( EmployeeFilterView.as_view() ,login_url='login'), name="search")
+    path("search",login_required( EmployeeFilterView.as_view() ,login_url='login'), name="search"),
+    path("notification",bday_alert ,name="bday")
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- emp/views.py
+++ emp/views.py
@@ -9,9 +9,23 @@ from django.contrib import messages
 from django.db.models import Q
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+import datetime
+from datetime import timedelta
+from django.utils import timezone
+from celery.schedules import crontab
+from celery.task import periodic_task
+
 
 from django.core.mail import send_mail
 # Create your views here.
+
+def get_bday_count():
+    datetime_now = timezone.now()
+    now_day, now_month = datetime_now.day, datetime_now.month
+    cnt = MyEmp.objects.filter(dob__day=now_day, dob__month=now_month).count()
+    return cnt
+
+
 @method_decorator(login_required,name="dispatch")
 class Home(TemplateView):
     template_name="index2.html"
@@ -23,12 +37,14 @@ class Home(TemplateView):
         femp = MyEmp.objects.filter(gender="Female").count()
         memp = MyEmp.objects.filter(gender="Male").count()
         oemp = MyEmp.objects.filter(gender="Others").count()
+        cnt=get_bday_count()
         self.context = {
             "totemp": totemp,
             "femp": femp,
             "memp":memp,
             "oemp":oemp,
-            "employees":employees
+            "employees":employees,
+            "cnt":cnt
         }
         return render(request, self.template_name, self.context)
 
@@ -55,7 +71,7 @@ class SignInView(TemplateView):
 
                 login(request,user)
                 print("loginsucess")
-                return redirect("home")
+                return redirect("bday")
 
 
             else:
@@ -79,6 +95,8 @@ class EmpCreationView(CreateView):
     form_class = EmployeeRegistrationForm
     template_name = "register.html"
     success_url = reverse_lazy("list")
+    cnt = get_bday_count()
+    context={"cnt":cnt}
 
 @method_decorator(login_required, name="dispatch")
 class EmpListView(TemplateView):
@@ -89,7 +107,9 @@ class EmpListView(TemplateView):
 
     def get(self,request,*args,**kwargs):
         employees=self.model.objects.all()
-        self.context["employees"]=employees
+        cnt = get_bday_count()
+        self.context={"employees":employees,"cnt":cnt}
+        # self.context["employees"]=employees
         return render(request, self.template_name, self.context)
 
 @method_decorator(login_required, name="dispatch")
@@ -108,10 +128,56 @@ class EmpEditView(UpdateView):
 @method_decorator(login_required, name="dispatch")
 class EmployeeFilterView(TemplateView):
     def post(self,request,*args,**kwargs):
+        cnt = get_bday_count()
+
         search=request.POST.get('search',None)
         if search:
             employees=MyEmp.objects.filter((Q(first_name=search) | Q(department=search) | Q(id=search)))
 
-            return render(request, "filter.html",{'employees':employees})
+            return render(request, "filter.html",{'employees':employees,'cnt':cnt})
         return render(request, "filter.html")
 
+# class BdayNotification(TemplateView):
+#     def get(self,request,*args,**kwargs):
+#         emp=MyEmp.objects.all()
+#         date = datetime.date.today()
+#         print(date)
+#         for em in emp:
+#
+#             # print(str(em.dob))
+#             bday=str(em.dob)
+#             bmd=bday[5:]
+#             print(bmd)
+#             tday=str((date))
+#             today=tday[5:]
+#             print(today)
+#
+#
+#         return render(request,"index2.html")
+# @periodic_task(run_every=crontab(hour=3, minute=57, day_of_week=6))
+# def bday_alert(request):
+#
+#     datetime_now = timezone.now()
+#     now_day, now_month = datetime_now.day, datetime_now.month
+#     print(now_day)
+#     print(now_month)
+#     emps = MyEmp.objects.filter(dob__day=now_day, dob__month=now_month)
+#     print("hi")
+#     print(emps)
+#     context={"emps":emps}
+#     return render(request,"index2.html",context)
+
+
+def bday_alert(request,*args,**kwargs):
+    if request.method=="GET":
+        datetime_now = timezone.now()
+        now_day, now_month = datetime_now.day, datetime_now.month
+        print(now_day)
+        print(now_month)
+        emps = MyEmp.objects.filter(dob__day=now_day, dob__month=now_month)
+        cnt = MyEmp.objects.filter(dob__day=now_day, dob__month=now_month).count()
+        print("hi")
+        print(emps)
+        context={"emps":emps,
+                 "cnt":cnt}
+        return render(request, "index2.html",context)
